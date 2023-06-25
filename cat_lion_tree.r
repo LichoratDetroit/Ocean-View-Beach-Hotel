95423b3e124fa35939873a7154f499a259d10498
LichoratDetroit <137534168+LichoratDetroit@users.noreply.github.com>
2023-06-26 03:00:24 +0700

    Add test coverage

---
+++ cat_lion_tree.r
@@ -0,0 +1,55 @@
+# code for a beachfront hotel
+
+#1 create the hotel object
+hotel <- list()
+
+#2 Initialize the hotel rooms
+hotel$rooms <- list()
+
+#3 Create the lobby
+hotel$lobby <- list()
+
+#4 Add amenities to the lobby
+hotel$lobby$amenities <- c('Cafe', 'Gift Shop', 'Free WiFi', 'Concierge Desk', 'Bar')
+
+#5 Create the dining area
+hotel$dining <- list()
+
+#6 Add amenities to the dining area
+hotel$dining$amenities <- c('All Day Dining', 'Outdoor Seating', 'Poolside Dining')
+
+#7 Create the pool
+hotel$pool <- list()
+
+#8 Add amenities to the pool
+hotel$pool$amenities <- c('Daybeds', 'Swim-up Bar', 'Poolside Cabanas')
+
+#9 Create the fitness center
+hotel$fitness <- list()
+
+#10 Add amenities to the fitness center
+hotel$fitness$amenities <- c('State-of-the-art Gym Equipment', 'Fitness Classes', 'Yoga Studio')
+
+#11 Create the spa
+hotel$spa <- list()
+
+#12 Add amenities to the spa
+hotel$spa$amenities <- c('Indoor & Outdoor Massage Areas', 'Sauna', 'Jacuzzi')
+
+#13 Create the beach
+hotel$beach <- list()
+
+#14 Add amenities to the beach
+hotel$beach$amenities <- c('Beach Chairs', 'Umbrellas', 'Beachside Dining')
+
+#15 Create the garden
+hotel$garden <- list()
+
+#16 Add amenities to the garden
+hotel$garden$amenities <- c('Outdoor Patio', 'Fire Pit', 'Outdoor Lounge Areas')
+
+#17 Create the business center
+hotel$business <- list()
+
+#18 Add amenities to the business center
+hotel$business$amenities <- c('Meeting Rooms', 'Office Equipment', 'Copying & Printing Services')
